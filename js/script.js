a9e3aa43b77130749a948c16fcd5e2a2735f57c9
Alec Raeside <alec@AlMac.local>
2012-03-10 19:24:21 +1100

    configure css structure & begin js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 /* Author:
 
 */
-
-
+$(function() {
+	var tiles = [],
+		$tiles = $("#tiles"),
+		num_tiles_wide = 3,
+		num_tiles_high = 4,
+		tile_width = 100,
+		tile_height = 80,
+		blank_x = Math.floor(Math.random()*num_tiles_wide),
+		blank_y = Math.floor(Math.random()*num_tiles_high);
+	for (var i=0;i<num_tiles_high;i++) {
+		var row = tiles[i];
+		for (var n=0;n<num_tiles_wide;n++) {
+			if (blank_x!==n || blank_y !== i) {
+				var tile = $("<div></div>").css({"left":(n*tile_width)+n,"top":(i*tile_height)+i})
+				$tiles.append(tile);
+			}
+		}
+	}
+	
+})
 
 
 
