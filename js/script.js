e305028d49e7834813643141799dc7a34ab4a79b
Alec Raeside <alec@AlMac.local>
2012-03-11 19:47:06 +1100

    Get consecutive sliding working
    change blank tile reference to actual DOM element. Reset drag
    containment when a tile is dragged to the blank tile coords

--- js/script.js
+++ js/script.js
@@ -13,41 +13,65 @@ $(function() {
 	for (var i=0;i<num_tiles_high;i++) {
 		tiles[i]=[];
 		for (var n=0;n<num_tiles_wide;n++) {
-			if (blank_x!==n || blank_y !== i) {
+			
 				var tile = $("<div></div>")
+				if (blank_x==n && blank_y ==i) {
+					tile.addClass("blank");
+				}	
 				tile.css({"left":(n*tile_width),"top":(i*tile_height)});
+				tile.data("coords",{"row":i,"column":n});
 				
 				$tiles.append(tile);
 				tiles[i][n]=tile;
-			} else {
-				tiles[i][n]="blank";
-			}
+			
 		}
 	}
 	//log(typeof tiles[1][45]);
 	var resetDragContainment = function() {
 		$.each(tiles,function(i,row) {
 			$.each(row,function(n,tile) {
-				if (tile!=="blank") {
+				if (!tile.hasClass("blank")) {
 					var contain_rect = [0,0,0,0];
-					if (i>0 && tiles[i-1][n]=="blank") {
+					if (i>0 && tiles[i-1][n].hasClass("blank")) {
 						contain_rect = [tile.position().left , tile.position().top-tile_height , tile.position().left,tile.position().top ];	
-					} else if (i<num_tiles_high-1 && tiles[i+1][n]=="blank") {
+					} else if (i<num_tiles_high-1 && tiles[i+1][n].hasClass("blank")) {
 						contain_rect = [tile.position().left , tile.position().top , tile.position().left,tile.position().top+tile_height ];	
-					} else if (n>0 && tiles[i][n-1]=="blank") {
+					} else if (n>0 && tiles[i][n-1].hasClass("blank")) {
 						contain_rect = [tile.position().left-tile_width , tile.position().top , tile.position().left,tile.position().top ];	
-					} else if (n<num_tiles_wide-1 && tiles[i][n+1]=="blank") {
+					} else if (n<num_tiles_wide-1 && tiles[i][n+1].hasClass("blank")) {
 						contain_rect = [tile.position().left , tile.position().top, tile.position().left+tile_width,tile.position().top ];	
 					} else {
 						contain_rect = [tile.position().left , tile.position().top, tile.position().left,tile.position().top ];	
 					}	
 					
 					tile.draggable("option","containment", contain_rect )
+						
 				}
 			})
 		})
 	}
-	$tiles.find("div").draggable()
+	log(tiles);
+	$tiles.find("div:not(.blank)").draggable({
+		snapTolerance:10,
+		snap:".blank",
+		stop:function(event,ui) {
+			var blank = $(".blank")
+			if (ui.offset.left == blank.position().left && ui.offset.top == blank.position().top) {
+				//tiles switched
+
+				var blank_coords = blank.data("coords");
+				var this_coords = $(this).data("coords")
+				blank.data("coords",this_coords);
+				$(this).data("coords",blank_coords);
+				tiles[blank_coords.row][blank_coords.column] = $(this);
+				tiles[this_coords.row][this_coords.column] = blank;
+				//log(this_coords.left,tile_width)
+				blank.css({"left":this_coords.column*tile_width,"top":this_coords.row*tile_height});
+
+				resetDragContainment();
+			}
+		}
+	})
 	resetDragContainment()
 })
 
